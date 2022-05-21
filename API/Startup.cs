90a0171827c14203007723bb78f0c6c98f443d43
unknown <ruslan.absalam@gmail.com>
2022-05-21 19:23:10 +0600

    end of section 6

--- API/Startup.cs
+++ API/Startup.cs
@@ -29,7 +29,14 @@ namespace API
                 x.UseSqlite(_config.GetConnectionString("DefaultConnection")));
 
             services.AddApplicationServices();
-            services.AddSwaggerDocumentation();          
+            services.AddSwaggerDocumentation();  
+            services.AddCors(opt => 
+            {
+                opt.AddPolicy("CorsPolicy", policy =>
+                {
+                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200");
+                });
+            });   
             
         }
 
@@ -45,6 +52,8 @@ namespace API
 
             app.UseRouting();
             app.UseStaticFiles(); 
+
+            app.UseCors("CorsPolicy");
 
             app.UseAuthorization(); 
 
